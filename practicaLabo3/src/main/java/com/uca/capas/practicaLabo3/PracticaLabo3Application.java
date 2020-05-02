30e71aa6d5bfca8deb3fd70ca2d3e0253447b05c
00120717 <00120717@uca.edu.sv>
2020-05-02 10:58:39 -0600

    First Commit

---
+++ practicaLabo3/src/main/java/com/uca/capas/practicaLabo3/PracticaLabo3Application.java
@@ -0,0 +1,13 @@
+package com.uca.capas.practicaLabo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaLabo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaLabo3Application.class, args);
+	}
+
+}
